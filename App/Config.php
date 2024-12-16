0a5774ef9bacff488035abe092a566070c81f54e
eestrada_21 <159752169+ErikTona@users.noreply.github.com>
2024-12-16 04:01:07 -0700

    Update Config.php

--- App/Config.php
+++ App/Config.php
@@ -1,2 +1,6 @@
-<?php define('BASE_PATH', 'https://proyectogestioncursos-production.up.railway.app/');?>
+<?php 
+ob_start();
+define('BASE_PATH', 'https://proyectogestioncursos-production.up.railway.app/');
+ob_end_flush();
+?>
 
